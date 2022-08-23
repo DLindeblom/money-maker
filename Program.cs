496057effa552437ca3806991fec7a80329b6ed0
DLindeblom <drdlindeblom@gmail.com>
2022-08-23 15:54:29 -0400

    start writing logic to convert amount to coins

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Money Maker!");
+
+
+Console.Write("Please enter an amount to see how many coins you will have: ");
+string input = Console.ReadLine();
+
+double amount = Convert.ToDouble(input);
+
+Console.WriteLine($"{amount} is equal to...");
+
