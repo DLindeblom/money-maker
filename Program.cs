025ecff0c33fade88207ce06387a794fe914852a
DLindeblom <drdlindeblom@gmail.com>
2022-08-24 10:07:53 -0400

    add logic to calculate the minimum number of coins

--- Program.cs
+++ Program.cs
@@ -9,3 +9,21 @@ double amount = Convert.ToDouble(input);
 
 Console.WriteLine($"{amount} is equal to...");
 
+int gold = 10;
+int silver = 5;
+
+double goldCoins = amount / gold;
+
+goldCoins = Math.Floor(goldCoins);
+
+double remainder = amount % gold;
+
+double silverCoins = remainder / silver;
+
+silverCoins = Math.Floor(silverCoins);
+
+remainder = remainder % silver;
+
+Console.WriteLine($"Gold Coins: {goldCoins}");
+Console.WriteLine($"Silver Coins: {silverCoins}");
+Console.WriteLine($"Bronze Coins: {remainder}");
